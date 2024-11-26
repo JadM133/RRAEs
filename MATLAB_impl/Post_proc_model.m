e60cc10b518fc06f373d8fe67d06cb3255745139
Jad Mounayer <123482935+JadM133@users.noreply.github.com>
2024-11-26 11:56:59 +0100

    Update Post_proc_model.m

--- MATLAB_impl/Post_proc_model.m
+++ MATLAB_impl/Post_proc_model.m
@@ -15,8 +15,14 @@ st.method = "Strong"; % Type of the model
 st = filter_strings(st);
 save("st.mat", "st")
 
+% Here you should specify where your python is for MATLAB to know. If you don't know where
+% it is, just run python (or python3) on a terminal and execute the following two commands:
+% import sys
+% print(sys.executable)
+% Then copy the output of this and put it in the following variable:
+python_loc = "C:\Users\jadmo\Desktop\bugs_RRAEs\.venv\Scripts\python"
+system(strcat(python_loc," M_post_proc_model.py st.mat"));
 % The results will be stored in res, and saved in .mat file.
-system("C:\Users\jadmo\Desktop\bugs_rraes\.venv\Scripts\python M_post_proc_model.py st.mat");
 fprintf("Coeffs are saved in coeffs.mat\n")
 
 function [S] = filter_strings(S) 
